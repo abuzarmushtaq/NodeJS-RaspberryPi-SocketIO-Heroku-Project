0f326ff1cd9da5bb51579fb9c7d018096edabd26
abuzarmushtaq <abuzarmushtaq2016@outlook.com>
2018-01-09 05:10:53 +0500

    testing

--- client/config.js
+++ client/config.js
@@ -8,7 +8,7 @@ config.led = 4;
 // locally on your network it might be something like
 // http://10.0.0.227:3000 where 10.0.0.227 is the IP address of 
 // the machine running the server code.
-config.server_url = "https://safe-everglades-56010.herokuapp.com";
+config.server_url = "https://radiant-hamlet-50517.herokuapp.com";
 
 if(config.led == -1 || config.server_url == ""){
   console.log("Before running app.js you must configure the LED pin number and server url in ./config.js");
